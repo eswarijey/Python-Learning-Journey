dbd39f0157b6d6a989da127b2138c688353fbc40
Eswari Jeyaram <eswarijeyaram15@gmail.com>
2026-04-08 00:27:29 +0100

    BMI Calculator

---
+++ Self_Learning/01_Basics/02_bmi_calculator.py
@@ -0,0 +1,37 @@
+# Author : Eswari
+# Description : BMI Calculator
+# Date : 07 Apr 2026
+
+def bmical(w,h):
+    bmi = w / (h ** 2)
+    return bmi
+
+def bmicateg(bmival):
+    if bmival < 18.5:
+        print("Underweight")
+    elif bmival<25:
+        print("Normal")
+    elif bmival<30:
+        print("Overweight")
+    else:
+        print("Obese")
+
+while True:
+    try:
+        weight = float(input("Enter weight in kg: "))
+        if not(0<weight<=300):
+            print("Invalid weight entered, the weight should from greater than 0 kg and should be less than or equal to 300 kg")
+            continue
+
+        height = float(input("Enter height in meters: "))
+        if not(0<height<=2.5):
+            print("Invalid height entered, the height should be greater than 0m and less than or equal to 2.5m")
+            continue
+
+        getbmi = bmical(weight,height)
+        getcateg = bmicateg(getbmi)
+
+        break
+    
+    except ValueError as ve:
+        print("Error :", ve)
